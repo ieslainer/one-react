60dfbc9ed604c88d35f47424f8e4852e99666523
Dejan Ljevar <dejan.ljevar@hmhco.com>
2017-02-09 16:19:08 +0100

    update of e2e conf

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,26 @@
 module.exports = function(grunt){
 
   require('load-grunt-config')(grunt, {});
-  
+
   grunt.registerTask('build', ['webpack:dev']);
   grunt.registerTask('serve', ['build', 'connect:server', 'watch:js']);
+  grunt.loadNpmTasks('grunt-webdriver');
+  grunt.loadNpmTasks('grunt-selenium-webdriver');
+  grunt.registerTask('e2e', [
+    'selenium_phantom_hub',
+    'mocha_e2e',
+    'selenium_stop'
+  ]);
 
+  grunt.initConfig({
+    webdriver: {
+      test: {
+          configFile: './test/wdio.conf.js'
+      },
+      selenium_start: {
+        options: { port: 4444 }
+    },
+    },
+    // ...
+  });
 };
